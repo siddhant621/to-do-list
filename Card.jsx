c8fab034ce42d681a3db028666f612584529ff39
siddhant <siddhantsaini098@gmail.com>
2025-11-10 11:39:11 +0530

    comiited

---
+++ Card.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Card.css"
+function Card(props) {
+  return (
+    <div>
+      <img src={props.image} alt={props.title} className='card-image' />
+      <h3 className='card-title'>{props.title}</h3>
+      <p className='card-description'>{props.description}</p>
+    </div>
+  );
+}
+
+export default Card;
